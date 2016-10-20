5d7a8e5ea8c308da73cafbb6aac1eb223a550168
Cheburusska <deminvictor@gmail.com>
2016-10-20 11:50:54 +0300

    Task done

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,25 @@
+import re
+from collections import Counter
 
 
-def load_data(filepath):
-    pass
+filepath = ('sample_text/text.txt')
 
 
-def get_most_frequent_words(text):
-    pass
+def load_data(filepath):#takes path to the txt file, returns lisf of words in lowercase without punctuation
+    with open(filepath, encoding="utf8") as text:
+        t = text.read().lower()
+        t_list = re.sub("[^\w,-]", " ",  t).split()
+        return t_list
+
+
+def get_most_frequent_words(text): #takes list of words, returns list of 10 most common words
+    return Counter(text).most_common(10)
+
+
+def main(): #prints result in console
+    text = load_data(filepath)
+    print (get_most_frequent_words(text))
 
 
 if __name__ == '__main__':
-    pass
+    main()
